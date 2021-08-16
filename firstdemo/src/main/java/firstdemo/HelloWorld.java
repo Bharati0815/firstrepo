3ab48e0801b457e8f05db9b73bf69fe42f982595
test <test@m8>
2021-08-16 13:56:58 +0530

    First Commit

---
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -0,0 +1,12 @@
+package firstdemo;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
